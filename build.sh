4789acb466ed2a3468f38c4215225939cf4427c1
root <aobersnel@apnex.com.au>
2019-09-20 10:36:11 +1000

    Updates..

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ mv layout.tar.gz ../
 cd ..
 
 # remove existing and rebuild image
-docker rmi -f apnex/bind-cli 2>/dev/null
-docker build --rm --no-cache -t apnex/bind-cli -f control-dns.docker .
+docker rmi -f apnex/control-dns 2>/dev/null
+docker build --rm --no-cache -t apnex/control-dns -f control-dns.docker .
 
 # remove temp layout
 rm layout.tar.gz
